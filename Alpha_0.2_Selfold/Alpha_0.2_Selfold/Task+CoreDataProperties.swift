4042e8f7237675affabc0cc875e48e1bac7f6aa1
Cristi Cretu <cristicretu38@gmail.com>
2021-07-08 20:01:57 +0300

    Fixes getDaylyTaskItems function

--- Alpha_0.2_Selfold/Alpha_0.2_Selfold/Task+CoreDataProperties.swift
+++ Alpha_0.2_Selfold/Alpha_0.2_Selfold/Task+CoreDataProperties.swift
@@ -9,7 +9,6 @@
 import Foundation
 import CoreData
 
-
 extension Task {
 
 //    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
@@ -34,28 +33,33 @@ extension Task : Identifiable {
         return request
     }
     
+    // function to create new date with same year,month, day as TODAY and time 0:0:0
+    // BASICALLY TODAY
+    static func createDailyDate(date : Date) -> Date {
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: date)
+        dateComponents.hour = 0
+        dateComponents.minute = 0
+        dateComponents.second = 0
+        guard let newDate = Calendar.current.date(from: dateComponents) else { return Date() }
+        return newDate
+    }
+    
     static func getDailyTaskItems() -> NSFetchRequest<Task> {
         let fetchRequest : NSFetchRequest<Task> = Task.fetchRequest() as! NSFetchRequest<Task>
-        fetchRequest.predicate = NSPredicate(format: "date >= %@", Date() as CVarArg)
+        fetchRequest.predicate = NSPredicate(format: "date >= %@ AND date < %@", Date().addingTimeInterval(-10 * 60) as CVarArg,     Calendar.current.date(byAdding: .day, value: 1, to: createDailyDate(date: Date()))! as CVarArg)
         let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
         return fetchRequest
     }
     
-//    static func getDailyTaskItems() -> NSFetchRequest<Task> {
-//        let request : NSFetchRequest<Task> = Task.fetchRequest() as! NSFetchRequest<Task>
-//        let filter = ???
-//        let predicate = NSPredicate(format = "", filter)
-//        request.predicate = predicate
-//        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
-//        request.sortDescriptors = [sortDescriptor]
-//        return request
-//    }
-    
-//    static func getAllPoints() -> Int {
-//        let request : NSFetchRequest<Task> = Task.fetchRequest() as! NSFetchRequest<Task>
-//        print(request)
-//        return 5
+//    static func getDailyTotalPoints() -> Int {
+//        let fetchRequest : NSFetchRequest<Task> = Task.fetchRequest() as! NSFetchRequest<Task>
+//        let newDate = createDailyDate(date: Date())
+//        fetchRequest.predicate = NSPredicate(format: "date >= %@", newDate as CVarArg)
+//
+//        for item in fetchRequest {
+//
+//        }
 //    }
 
 //    static func getCompletedPoints() -> NSFetchRequest<Task> {
